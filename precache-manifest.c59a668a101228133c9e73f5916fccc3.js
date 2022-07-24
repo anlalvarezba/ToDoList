55f0ba3212285dd8ad7cdd86427b1e6e39797c9c
anlalvarezba <anlalvarezba@unal.edu.co>
2022-07-23 23:12:57 -0500

    Updates

---
+++ precache-manifest.c59a668a101228133c9e73f5916fccc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c260c730908ec709fd78",
+    "url": "/ToDoList/static/css/main.c4110c6f.chunk.css"
+  },
+  {
+    "revision": "c260c730908ec709fd78",
+    "url": "/ToDoList/static/js/main.c260c730.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/ToDoList/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "b0420226dc1dc1a6d39c",
+    "url": "/ToDoList/static/js/runtime~main.b0420226.js"
+  },
+  {
+    "revision": "6934f2a6fbd002b56a6778a994d2e9ae",
+    "url": "/ToDoList/static/media/check.6934f2a6.svg"
+  },
+  {
+    "revision": "257ddb03563fc9649d4cb84b97f66a74",
+    "url": "/ToDoList/static/media/delete.257ddb03.svg"
+  },
+  {
+    "revision": "666db58f192ef2be0ac4d4fdb02a26ef",
+    "url": "/ToDoList/index.html"
+  }
+];
