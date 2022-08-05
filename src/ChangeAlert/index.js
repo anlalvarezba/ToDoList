2fe5f08483e0e58411d46cdd18a30ff44977eb65
anlalvarezba <anlalvarezba@unal.edu.co>
2022-08-04 22:47:28 -0500

    Adding styles

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import './changeAlert.css';
 
-function ChangeAlert({show, toggleShow}){
+function ChangeAlert({ show, toggleShow}) {
     if(show){
         return (
-            <div>
-                <p>Hay cambios sin actualizar.</p>
+            <div className="ChangeAlert-bg">
+                <div className="ChangeAlert-container">
+                <p>Parece que cambiaste tus TODOs en otra pestaña o ventana del navegador.</p>
+                <p>¿Quieres sincronizar tus TODOs?</p>
                 <button
+                    className="TodoForm-button TodoForm-button--add"
                     onClick={toggleShow}
                 >
-                    Refresca la pagina
+                    Yes!
                 </button>
+                </div>
             </div>
         );
-    } else {
+    } else{
         return null;
     }
 }
